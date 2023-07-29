9817444ec625f16a7bd675918211572aa64cfeab
Fabio Magagnin <fmagagnin@fullstacklabs.co>
2023-07-29 11:07:31 -0300

    Code cleanup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const CAR = 'car'
 const GOAT = 'goat'
 const doors = [GOAT, GOAT, CAR]
+const RUNS = 1000000
 
 const randomPick = (min, max) => {
   const range = max - min + 1;
@@ -8,7 +9,6 @@ const randomPick = (min, max) => {
 }
 
 const runGame = (switchDoor) => {
-    // checkPseudoRandomDistribution()
     let concurrentPick = randomPick(0, 2)
     let hostPick = null
 
@@ -26,54 +26,31 @@ const runGame = (switchDoor) => {
         hostPick = otherOption
     }
 
-
-    // console.log(' concurrent: ', concurrentPick)
     if (switchDoor) {
         const otherOption = [0, 1, 2].filter(item => item !== concurrentPick && item != hostPick)[0]
         concurrentPick = otherOption
-        // console.log('       host: ', hostPick)
-        // console.log(' concurrent: ', concurrentPick)
-        // console.log('otherOption: ', otherOption)
-        // console.log('SWITCHED')
     }
 
     const concurrentWon = doors[concurrentPick] === 'car'
     return concurrentWon
 }
 
-
+const runSample = (switchDoor) => {
+    let wins = 0
+    let losts = 0
+    for (i = 0; i < RUNS; i++) {
+        const won = runGame(switchDoor)
+        won ? wins++ : losts++
+    }
+    console.log(switchDoor ? '\n--- Switching the door ---' : '--- Not switching the door ---')
+    console.log(` runs: ${RUNS}`)
+    console.log(` wins: ${wins} (${ wins / RUNS })`)
+    console.log(`losts: ${losts} (${ losts / RUNS})`)
+}
 
 const init = () => {
     runSample(false)
     runSample(true)
-}
-
-const RUNS = 1000000
-
-const runSample = (switchDoor) => {
-    let wons = 0
-    let losts = 0
-    for (i = 0; i < RUNS; i++) {
-        const won = runGame(switchDoor)
-        won ? wons++ : losts++
-    }
-    console.log(switchDoor ? 'SWITCHING': 'NOT SWITCHING')
-    console.log(` wons: ${wons} (${wons/RUNS})`)
-    console.log(`losts: ${losts} (${losts/RUNS})`)
-    
-}
-
-const checkPseudoRandomDistribution = () => {
-    const SAMPLE = 30000000
-    const map = { 0:0, 1:0, 2:0 }
-    for (let i = 0; i < SAMPLE; i++) {
-        const result = randomPick(0, 2)
-        map[result]++
-    }
-    console.log(map)
-    console.log('0', map[0]/SAMPLE)
-    console.log('1', map[1]/SAMPLE)
-    console.log('2', map[2]/SAMPLE)
 }
 
 init()
